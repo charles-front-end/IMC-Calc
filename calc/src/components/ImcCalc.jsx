fe46b9525d64e8458e2c7679a053c532dd31122f
Charles Silva <charlesk12019@gmail.com>
2025-11-12 21:51:22 -0300

    IMC-Calc

---
+++ calc/src/components/ImcCalc.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+import "./ImcCalc.css";
+import Button from "./button";
+
+const ImcCalc = ({ calcImc }) => {
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+
+  const clearForm = (e) => {
+    e.preventDefaul();
+    setWeight("");
+    setHeight("");
+  };
+
+  const validDigits = (text) => {
+    return text.replace(/[^0-9,]/g, "");
+  };
+
+  const handleHeightChange = (e) => {
+    const updatedValue = validDigits(e.target.value);
+
+    setHeight(updatedValue);
+  };
+
+  const handleWeightChange = (e) => {
+    const updatedValue = validDigits(e.target.value);
+
+    setWeight(updatedValue);
+  };
+
+  return (
+    <div id="calc-container">
+      <h2>Calculadora de IMC</h2>
+      <form id="imc-form">
+        <div className="form-inputs">
+          <div className="form-control">
+            <label htmlFor="height">Altura:</label>
+            <input
+              type="text"
+              name="height"
+              id="height"
+              placeholder="Em metros (ex: 1.75)"
+              step="0.01"
+              required
+              onChange={(e) => handleHeightChange(e)}
+              value={height}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="weight">Peso (Kg):</label>
+            <input
+              type="text"
+              name="weight"
+              id="weight"
+              placeholder="Em Peso (ex: 70,5)"
+              step="0.001"
+              required
+              onChange={(e) => handleWeightChange(e)}
+              value={weight}
+            />
+          </div>
+        </div>
+        <div className="action-control">
+          <Button
+            id="calc-btn"
+            text="Calcular"
+            action={(e) => calcImc(e, height, weight)}
+          />
+          <Button id="clear-btn" text="Limpar" action={clearForm} />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ImcCalc;
